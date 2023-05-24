7092802a5aa70f168ceb99828281f894bece46ed
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-05-24 11:55:32 -0400

    Added project

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by M_2195552 on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
